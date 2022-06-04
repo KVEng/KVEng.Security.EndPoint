f349a9ffaa87f96482eb39302016049f2cf6ed12
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-06-04 16:47:58 +0100

    fix: foreground

--- KVEng.Security.EndPoint.WpfClient/TaskViewDisabler.cs
+++ KVEng.Security.EndPoint.WpfClient/TaskViewDisabler.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
@@ -9,6 +11,26 @@ class TaskViewDisabler
     [return: MarshalAs(UnmanagedType.Bool)]
     static extern bool SetForegroundWindow(IntPtr hWnd);
 
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetForegroundWindow();
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+    private static void KeyboardHelp(int key, int oper)
+    {
+        keybd_event((byte)key, 0, oper, 0);
+    }
+
+    private static void PressKey(int key)
+    {
+        KeyboardHelp(key, KEYEVENTF_KEYDOWN);
+        KeyboardHelp(key, KEYEVENTF_KEYUP);
+    }
+
+    public const int KEYEVENTF_KEYDOWN = 0x0000;
+    public const int KEYEVENTF_KEYUP = 0x0002;
+    public const int VK_ESCAPE = 0x1B;              // ESC
+
     public static void DisableTaskView(IntPtr handle)
     {
         SetForegroundWindow(handle);
@@ -16,15 +38,16 @@ class TaskViewDisabler
 
     Timer _timer;
 
-    public  TaskViewDisabler (int time, IntPtr handle)
+    public TaskViewDisabler(int time, IntPtr handle)
     {
         _timer = new Timer();
         _timer.Interval = time;
         _timer.Tick += (o, e) =>
         {
+            if (GetForegroundWindow() != handle)
+                PressKey(VK_ESCAPE);
             SetForegroundWindow(handle);
         };
-
     }
 
     public void Start()
