1e978e39db1f122f7aeefd17864b78ba12d370bf
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-06-04 14:20:54 +0100

    feat: support multiple build

--- publish.bat
+++ publish.bat
@@ -1,6 +1,9 @@
 @ECHO OFF
 dotnet publish KVEng.Security.EndPoint.WpfClient -c Release -o publish -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true --runtime win10-x64 --no-self-contained -v normal
+MOVE publish\KVEng.Security.EndPoint.WpfClient.exe publish\KSE-win10-x64.exe
 
+dotnet publish KVEng.Security.EndPoint.WpfClient -c Release -o publish -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true --runtime win10-x64 --self-contained true
+MOVE publish\KVEng.Security.EndPoint.WpfClient.exe publish\KSE-c-win10-x64.exe
 PAUSE
 
 REM -p:PublishTrimmed=false     NOT SUPPORTED
