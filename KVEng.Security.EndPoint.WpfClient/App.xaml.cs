c73c3519de6724cf0617e0dc98ae85c2cb705a9a
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-06-04 15:40:29 +0100

    feat: support one instance

--- KVEng.Security.EndPoint.WpfClient/App.xaml.cs
+++ KVEng.Security.EndPoint.WpfClient/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -15,13 +17,33 @@ namespace KVEng.Security.EndPoint.WpfClient
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            if (HaveSameNameProcess())
+            {
+                MessageBox.Show("You have started one KSE, exiting...");
+                Quit();
+                return;
+            }
+
             base.OnStartup(e);
             var form = new SettingForm();
             form.Show();
         }
 
+        private static bool HaveSameNameProcess()
+        {
+            var cur = Process.GetCurrentProcess();
+            var procs = Process.GetProcessesByName(cur.ProcessName);
+            foreach (var p in procs)
+            {
+                if (p.Id != cur.Id)
+                    return true;
+            }
+            return false;
+        }
+
         public static void Quit()
         {
+            Unkillable.MakeProcessKillable();
             Application.Current.Shutdown();
         }
     }
