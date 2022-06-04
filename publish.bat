c820c5cfd38eaece38d01ca9dec334637bed7923
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-06-04 14:16:21 +0100

    feat: new publish script

--- publish.bat
+++ publish.bat
@@ -1,4 +1,8 @@
 @ECHO OFF
-dotnet publish -c Release -o publish -p:PublishReadyToRun=true -p:PublishSingleFile=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true --runtime win10-x64
+dotnet publish KVEng.Security.EndPoint.WpfClient -c Release -o publish -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true --runtime win10-x64 --no-self-contained -v normal
+
 PAUSE
-REM -p:PublishTrimmed=false 
+
+REM -p:PublishTrimmed=false     NOT SUPPORTED
+REM --self-contained true       DO NOT CONTAINES .NET
+REM -p:PublishReadyToRun=true   DO NOT AOT
